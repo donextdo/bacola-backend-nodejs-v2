c2ae74df46b95acf0ef763c6e225fa696281b149
sudheerasrinath <sudheerasrinath99@gmail.com>
2023-04-06 14:58:18 +0530

    order feature

--- controllers/order.js
+++ controllers/order.js
@@ -37,6 +37,97 @@ try {
 };
 
 
+
+
+//get all orders
+const getAllOrders = async (req, res) => {
+  try {
+    let orders = await Order.find();
+    if (products) {
+      return res.json(orders);
+    } else {
+      return res
+        .status(404)
+        .send({ message: "Error occured when retrieving orders" });
+    }
+  } catch (err) {
+    return res.status(500).send({ message: "Internal server error" });
+  }
+};
+
+
+//get order by id
+const getOrderById = async (req, res) => {
+  const orderId = req.params.id;
+  
+
+  try {
+    let response = await Order.findById(orderId);
+  
+    if (response) {
+      return res.json(response);
+    } else {
+      return res.status(404).send({ message: "No such order found" });
+    }
+  } catch (err) {
+    return res.status(404).send({ message: "No such order found" });
+  }
+};
+
+//update order by id
+
+const updateOrder = async (req, res) => {
+  const Id = req.params.id;
+
+  let orderUpdate = {
+     orderId : req.body.orderId,
+     userId : req.body.userId,
+     item : req.body.item,
+     date : req.body.date,
+     total : req.body.total,
+     status : req.body.status,
+     createdAt : new Date(),
+     deletedAt : null,
+   
+  };
+
+  try {
+    const response = await Order.findOneAndUpdate({ _id: Id }, orderUpdate);
+    
+    if (response) {
+      return res.status(200).send({ message: "Successfully updated Order " });
+    } else {
+      return res.status(500).send({ message: "Internal server error" });
+    }
+  } catch (err) {
+    return res.status(400).send({ message: "Unable to update" });
+  }
+};
+
+
+//delete order by id
+const deleteOrder = async (req, res) => {
+  const Id = req.params.id;
+  try {
+    const response = await Order.findByIdAndDelete({ _id: Id });
+    if (response) {
+      return res
+        .status(204)
+        .send({ message: "Successfully deleted a Order" });
+    } else {
+      return res.status(500).send({ message: "Internal server error" });
+    }
+  } catch (err) {
+    console.log(err.message);
+    return res.status(400).send({ message: "Could not delete the request" });
+  }
+};
+
+
 module.exports = {
-    createOrder
+  createOrder,
+  getAllOrders,
+  getOrderById,
+  updateOrder,
+  deleteOrder,
 };
--- routes/order.js
+++ routes/order.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-let productController = require("../controllers/order");
+let orderController = require("../controllers/order");
 
-router.post("/place", orderController.addOrder);
+router.post("/place", orderController.createOrder);
+router.get("/", orderController.getAllOrders);
+router.get("/:id", orderController.getOrderById);
+router.put("/:id", orderController.updateOrder);
+router.delete("/:id", orderController.deleteOrder);
+
 
 module.exports = router;
