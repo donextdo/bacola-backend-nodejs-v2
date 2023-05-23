6fa52e365a0e2762d1ac8a4bbeb5577e709ca425
Thisara Thathsarani <thisararpg@gmail.com>
2023-05-23 13:15:48 +0530

    change filter option with pagination

--- controllers/category .js
+++ controllers/category .js
@@ -73,65 +73,6 @@ const getParent = async (req, res) => {
   }
 };
 
-// async function insertCategory(name, parentId) {
-//   try {
-//     const category = new Category({
-//       name: name,
-//       parent: parentId,
-//     });
-
-//     const savedCategory = await category.save();
-//     if (savedCategory) {
-//       const product = await Product.findById(parentId);
-
-//       if (product) {
-//         const categoryIds = product.category;
-//         categoryIds.push(savedCategory._id); // add new category id to the category array
-
-//         const productUpdate = {
-//           ...product._doc,
-//           category: categoryIds, // replace the category array with updated categoryIds
-//           updatedAt: new Date(),
-//         };
-
-//         try {
-//           const response = await Product.updateOne(
-//             { _id: parentId },
-//             productUpdate
-//           );
-
-//           if (response) {
-//             return { message: "Successfully updated Product" };
-//           } else {
-//             return { error: "Internal server error" };
-//           }
-//         } catch (err) {
-//           console.log("errror", err);
-//           return { error: "Unable to update" };
-//         }
-//       } else {
-//         return { error: "Product not found" };
-//       }
-//     }
-//     console.log(`Created category: ${savedCategory.name}`);
-//     return savedCategory;
-//   } catch (error) {
-//     console.error(`Error creating category: ${error.message}`);
-//     throw error;
-//   }
-// }
-
-// const categoryInsert = async (req, res) => {
-//   const { name, parentId } = req.body;
-
-//   try {
-//     const savedCategory = await insertCategory(name, parentId);
-//     res.status(201).json(savedCategory);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
 module.exports = {
   //categoryInsert,
   insertCategory,
--- controllers/productAdditional.js
+++ controllers/productAdditional.js
@@ -6,6 +6,9 @@ const getproductByfilter = async (req, res) => {
   const baseUrl = "http://localhost:4000/api";
 
   try {
+    const { sort, page = 1, perpage = 12 } = req.query;
+    const skip = (page - 1) * perpage;
+
     const categoryId = req.query.categoryId;
     const subCategories = req.query.subCategories;
     const brands = req.query.brands;
@@ -44,12 +47,93 @@ const getproductByfilter = async (req, res) => {
       products = products.filter((product) => product.onSale === true);
     }
 
-    res.status(200).json(products);
+    switch (sort) {
+      case "popularity":
+        products = products.sort((a, b) => b.popularity - a.popularity);
+        break;
+      case "rating":
+        products = products.sort((a, b) => b.averageRating - a.averageRating);
+        break;
+      case "date":
+        products = products.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        );
+        break;
+      case "price":
+        products = products.sort((a, b) => a.price - b.price);
+        break;
+      case "price-desc":
+        products = products.sort((a, b) => b.price - a.price);
+        break;
+      default:
+        break;
+    }
+
+    const paginatedProducts = products.slice(skip, skip + perpage);
+
+    const count = products.length;
+
+    const responses = {
+      products: paginatedProducts,
+      currentPage: parseInt(page),
+      totalPages: Math.ceil(count / perpage),
+      totalItems: count,
+    };
+
+    res.status(200).json(responses);
   } catch (error) {
     console.log("error : ", error);
     res.status(500).json({ message: "Server Error" });
   }
 };
+// const getproductByfilter = async (req, res) => {
+//   const baseUrl = "http://localhost:4000/api";
+
+//   try {
+//     const categoryId = req.query.categoryId;
+//     const subCategories = req.query.subCategories;
+//     const brands = req.query.brands;
+//     const minPrice = parseFloat(req.query.min_price);
+//     const maxPrice = parseFloat(req.query.max_price);
+//     const inStock = req.query.stock_status;
+//     const onSale = req.query.on_sale;
+
+//     const subCatArr =
+//       typeof subCategories === "string" ? subCategories.split(",") : [];
+
+//     const brandArr = typeof brands === "string" ? brands.split(",") : [];
+
+//     const url =
+//       subCatArr.length > 0
+//         ? `${baseUrl}/products/${subCatArr}`
+//         : `${baseUrl}/products/${categoryId}`;
+//     const response = await axios.get(url);
+
+//     let products = response.data;
+//     if (brandArr.length > 0) {
+//       products = products.filter((product) => brandArr.includes(product.brand));
+//     }
+
+//     if (!isNaN(minPrice) && !isNaN(maxPrice)) {
+//       products = products.filter(
+//         (product) => product.price >= minPrice && product.price <= maxPrice
+//       );
+//     }
+
+//     if (inStock === "true") {
+//       products = products.filter((product) => product.inStock === true);
+//     }
+
+//     if (onSale === "true") {
+//       products = products.filter((product) => product.onSale === true);
+//     }
+
+//     res.status(200).json(products);
+//   } catch (error) {
+//     console.log("error : ", error);
+//     res.status(500).json({ message: "Server Error" });
+//   }
+// };
 
 module.exports = {
   getproductByfilter,
